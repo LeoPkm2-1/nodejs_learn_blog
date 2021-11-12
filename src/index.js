48fc563866d2637f8192c3ad2bef95b38b049834
leoPkm <thinhphatmai2001@gmail.com>
2021-11-12 13:09:13 +0700

    v2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
-app.get('/about', (req, res) => {
+app.use(morgan('combined'))
+app.get('/', (req, res) => {
   let a=1;
   let b=5;
   let c=a*b;
-  res.send('Hello World!')
+  res.send("<h1 style='color:red'>ahihi everybody</h1>")
 })
 
 app.listen(port, () => {
